d819a6716a6a90d79c445d84c3dabc087841a188
ruggedev <ruggedev@gmail.com>
2023-08-04 22:34:07 +0800

    update deploy scripts

--- deploy/001_Ballot.ts
+++ deploy/001_Ballot.ts
@@ -1,27 +1,20 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { ethers } from 'ethers'
+import _ from 'lodash'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
 
-  // const proposals = [ethers.toUtf8Bytes('Proposal1'), ethers.toUtf8Bytes('Proposal2')]
+  const proposals = _.range(10).map((i) => {
+    return ethers.encodeBytes32String('Proposal' + i)
+  })
+
   const result = await deploy('Ballot', {
     from: deployer,
-    args: [
-      [
-        ethers.encodeBytes32String('Proposal1'),
-        ethers.encodeBytes32String('Proposal2'),
-        ethers.encodeBytes32String('Proposal3'),
-        ethers.encodeBytes32String('Proposal4'),
-        ethers.encodeBytes32String('Proposal5'),
-        ethers.encodeBytes32String('Proposal6'),
-        ethers.encodeBytes32String('Proposal7'),
-        ethers.encodeBytes32String('Proposal8'),
-      ],
-    ],
+    args: [proposals],
     log: true,
     skipIfAlreadyDeployed: true,
   })
